d05a23ac92f64c7ea4e02cf02c78a8d4f00e51c6
Danyel Cabello <danyel.nerv@gmail.com>
2016-07-10 17:22:30 -0500

    Correct index page

--- peoplesearch/urls.py
+++ peoplesearch/urls.py
@@ -6,8 +6,10 @@ admin Django standard admin site
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 
 urlpatterns = [ 
+    url(r'^$', lambda r: HttpResponseRedirect('api/')),
     url(r'^api/', include('api.urls')),
     url(r'^admin/', admin.site.urls),
 ]
