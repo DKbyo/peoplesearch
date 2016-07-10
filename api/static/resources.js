e53b1286404cbeb3f584f857329df9b8ca5c7861
Danyel Cabello <danyel.nerv@gmail.com>
2016-07-10 17:30:59 -0500

    Resources with relative urls

--- api/static/resources.js
+++ api/static/resources.js
@@ -1,6 +1,6 @@
 angular.module("SearchApp")
 .factory('Search', ['$resource', function($resource) {
-	return $resource('http://127.0.0.1:8000/api/people/', null,
+	return $resource('/api/people/', null,
 	{
 		get: { method:'GET',isArray:true,cancellable:true,params:{
 			format:"json"		
